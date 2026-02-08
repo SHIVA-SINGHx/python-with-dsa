358e2bf0b1c0a5b7f13087ac05b1604db5b5b6fa
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-02-08 19:02:27 +0530

    learned about BFS

--- Graph/BFS.py
+++ Graph/BFS.py
@@ -1,6 +1,8 @@
+from collections import deque
+
 class Graph:
     def __init__(self, vertex):
-        self.mat = [ [0]*vertex for x in range(vertex)]
+        self.mat = [ [0]*vertex for x in range(vertex)] ## jitne vertex honge like(8) utne kaa matrix ban jayga and sbme 0 fill ho jayga
         self.size = vertex
 
         
@@ -12,11 +14,34 @@ class Graph:
             
         else:
             print("Invalid Edge")
-            
-    def print(self):
-        for row in self.mat:
-           print(' '.join(map(str, row)))
            
-    def bfs(self, source):
-        return
+    def BFS(self, source):
+        visited_arr = [False] * self.size # jitne size ka element honge sb mai false fill ho jayga
+        queue = deque([source])
+        visited_arr[source] = True
+        
+        while(queue):
+            v = queue.popleft()
+            print(v, end=" ")
+            
+            for i in range(self.size):
+                if (self.mat[v][i] == 1 and visited_arr[i] == False):
+                    visited_arr[i] = True
+                    queue.append(i)
+                    
+g = Graph(8) ## how many vertex in there here's 8
+g.add_edge(0,1) # 0 to 1 connected
+g.add_edge(0,3) # 0 to 3 connected
+g.add_edge(1,3) # 1 to 3 connected
+g.add_edge(3,5) # 3 to 5 connected
+g.add_edge(3,4) # 3 to 4 connected
+g.add_edge(4,5) # 4 to 5 connected
+g.add_edge(4,6) # 4 to 6 connected
+g.add_edge(6,2) # 6 to 2 connected
+g.add_edge(6,7) # 6 to 7 connected
+
+g.BFS(0) ## start from 0
+            
+        
+        
         
